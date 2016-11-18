485ee2bfcab916192767ee0b935102a62fffb850
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-11-18 20:19:57 +0100

    Gotowy kot

---
+++ kot.cpp
@@ -0,0 +1,417 @@
+/*************************************************************************************/
+//  Szkielet programu do tworzenia modelu sceny 3-D z wizualizacją osi  
+//  układu współrzednych
+/*************************************************************************************/
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glut.h>
+#include <cmath>
+#include <iostream>
+
+#define M_PI 3.14159265358979323846
+
+using namespace std;
+
+typedef float point3[3];
+/*************************************************************************************/
+// Funkcja rysująca osie układu współrzędnych
+
+void Axes(void)
+{
+	point3  x_min = { -5.0, 0.0, 0.0 };
+	point3  x_max = { 5.0, 0.0, 0.0 };
+	// początek i koniec obrazu osi x
+
+	point3  y_min = { 0.0, -5.0, 0.0 };
+	point3  y_max = { 0.0,  5.0, 0.0 };
+	// początek i koniec obrazu osi y
+
+	point3  z_min = { 0.0, 0.0, -5.0 };
+	point3  z_max = { 0.0, 0.0,  5.0 };
+	//  początek i koniec obrazu osi y
+	glColor3f(1.0f, 0.0f, 0.0f);  // kolor rysowania osi - czerwony
+	glBegin(GL_LINES); // rysowanie osi x
+	glVertex3fv(x_min);
+	glVertex3fv(x_max);
+	glEnd();
+
+	glColor3f(0.0f, 1.0f, 0.0f);  // kolor rysowania - zielony
+	glBegin(GL_LINES);  // rysowanie osi y
+	glVertex3fv(y_min);
+	glVertex3fv(y_max);
+	glEnd();
+
+	glColor3f(0.0f, 0.0f, 1.0f);  // kolor rysowania - niebieski
+	glBegin(GL_LINES); // rysowanie osi z
+	glVertex3fv(z_min);
+	glVertex3fv(z_max);
+	glEnd();
+}
+/*************************************************************************************/
+// Funkcja określająca co ma być rysowane (zawsze wywoływana gdy trzeba 
+// przerysować scenę)
+
+float wyliczX(float u, float v) {
+	float PIV = M_PI * v;
+	return ((-90 * pow(u, 5) + 225 * pow(u, 4) - 270 * pow(u, 3) + 180 * pow(u, 2) - 45 * u) * cos(PIV));
+}
+float wyliczY(float u, float v) {
+	return (160 * pow(u, 4) - 320 * pow(u, 3) + 160 * pow(u, 2));
+}
+float wyliczZ(float u, float v) {
+	float PIV = M_PI  * v;
+	return ((-90 * pow(u, 5) + 225 * pow(u, 4) - 270 * pow(u, 3) + 180 * pow(u, 2) - 45 * u) * sin(PIV));
+}
+
+struct Punkt {
+	float x;
+	float y;
+	float z;
+};
+
+
+
+/********************************OBSŁUGA********************************/
+// 1- punkty, 2- siatka, 3 - wypełnione trójkąty, 4 - czjniczek
+int model = 1;
+int ruch = 1;
+float KAT = 60.0;
+
+// Ilosc podzialow boku kwadratu jednostkowego
+const int N = 150;
+// Obrot
+static GLfloat theta[] = { 0.0, 0.0, 0.0 };
+// Tablica sluzaca do zapisywania punktow
+struct Punkt punktyJaja[N][N];
+struct Punkt koloryJaja[N][N];
+
+float krok = 1.0 / N;
+
+/********************************KONIEC********************************/
+
+void spinEgg()
+{
+
+	theta[0] -= 0.5;
+	if (theta[0] > 360.0) theta[0] -= 360.0;
+
+	theta[1] -= 0.5;
+	if (theta[1] > 360.0) theta[1] -= 360.0;
+
+	theta[2] -= 0.5;
+	if (theta[2] > 360.0) theta[2] -= 360.0;
+
+	glutPostRedisplay(); //odświeżenie zawartości aktualnego okna
+}
+
+void nic() {
+	// Liczenie Punktow
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			punktyJaja[i][j].x = wyliczX(i * krok, j * krok);
+			punktyJaja[i][j].y = wyliczY(i * krok, j * krok);
+			punktyJaja[i][j].z = wyliczZ(i * krok, j * krok);
+		}
+	}
+	// Kolorki
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			koloryJaja[i][j].x = ((double)rand() / (RAND_MAX));
+			koloryJaja[i][j].y = ((double)rand() / (RAND_MAX));
+			koloryJaja[i][j].z = ((double)rand() / (RAND_MAX));
+		}
+	}
+}
+
+void RenderScene(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Czyszczenie okna aktualnym kolorem czyszczącym
+
+	glLoadIdentity();
+	// Czyszczenie macierzy bieżącej
+	Axes();
+
+	if (ruch == 1) {
+		glRotatef(theta[0], 1.0, 0.0, 0.0);
+	}
+	else if (ruch == 2) {
+		glRotatef(theta[1], 0.0, 1.0, 0.0);
+	}
+	else if (ruch == 3) {
+		glRotatef(theta[2], 0.0, 0.0, 1.0);
+	}
+	else {
+
+	}
+
+	// Narysowanie osi przy pomocy funkcji zdefiniowanej wyżej
+	glColor3f(1.0f, 1.0f, 0.0f); // Ustawienie koloru rysowania na biały
+
+	glRotated(KAT, 1.0, 1.0, 1.0);  // Obrót o 60 stopni
+
+	// Punkty
+	if (model == 1) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				glBegin(GL_POINTS);
+					glVertex3f(punktyJaja[i][j].x, punktyJaja[i][j].y - 5, punktyJaja[i][j].z);
+				glEnd();
+				//cout << "x[" << i << "," << j << "]" << ": " << punktyJaja[i][j].x << " y: " << punktyJaja[i][j].y - 5 << " z: " << punktyJaja[i][j].z << std::endl;
+			}
+		}
+	}
+	// Linie
+	else if (model == 2) {
+		int pom = 0;
+		int min = 1;
+		int max = N - 1;
+		int max1 = max - 1;
+		glBegin(GL_LINES);
+		while (pom != N + 1) {
+			int temp1 = N - pom + 1;
+			if (temp1 < max) {
+				for (int i = 1; i < N - 1; i++) {
+					// Poziom
+					glVertex3f(punktyJaja[temp1][i].x, punktyJaja[temp1][i].y - 5, punktyJaja[temp1][i].z);
+					glVertex3f(punktyJaja[temp1][i + 1].x, punktyJaja[temp1][i + 1].y - 5, punktyJaja[temp1][i + 1].z);
+
+					// Pion
+					glVertex3f(punktyJaja[temp1][i].x, punktyJaja[temp1][i].y - 5, punktyJaja[temp1][i].z);
+					glVertex3f(punktyJaja[temp1 + 1][i].x, punktyJaja[temp1 + 1][i].y - 5, punktyJaja[temp1 + 1][i].z);
+
+					// Skos
+					glVertex3f(punktyJaja[temp1][i].x, punktyJaja[temp1][i].y - 5, punktyJaja[temp1][i].z);
+					glVertex3f(punktyJaja[temp1 + 1][i + 1].x, punktyJaja[temp1 + 1][i + 1].y - 5, punktyJaja[temp1 + 1][i + 1].z);
+				}
+			}
+			else if (temp1 == max) {
+				for (int i = 1; i < N - 1; i++) {
+					// Poziom
+					glVertex3f(punktyJaja[temp1][i].x, punktyJaja[temp1][i].y - 5, punktyJaja[temp1][i].z);
+					glVertex3f(punktyJaja[temp1][i + 1].x, punktyJaja[temp1][i + 1].y - 5, punktyJaja[temp1][i + 1].z);
+
+					// Pion
+					glVertex3f(punktyJaja[temp1][i].x, punktyJaja[temp1][i].y - 5, punktyJaja[temp1][i].z);
+					glVertex3f(punktyJaja[0][0].x, punktyJaja[0][0].y - 5, punktyJaja[0][0].z);
+				}
+			}
+			int temp2 = pom + 1;
+			if (temp2 < (N - 2)) {
+				for (int i = 1; i < N - 1; i++) {
+					// Poziom
+					glVertex3f(punktyJaja[temp2][i].x, punktyJaja[temp2][i].y - 5, punktyJaja[temp2][i].z);
+					glVertex3f(punktyJaja[temp2][i + 1].x, punktyJaja[temp2][i + 1].y - 5, punktyJaja[temp2][i + 1].z);
+
+					// Pion
+					glVertex3f(punktyJaja[temp2][i].x, punktyJaja[temp2][i].y - 5, punktyJaja[temp2][i].z);
+					glVertex3f(punktyJaja[temp2 - 1][i].x, punktyJaja[temp2 - 1][i].y - 5, punktyJaja[temp2 - 1][i].z);
+
+					// Skos
+					glVertex3f(punktyJaja[temp2][i].x, punktyJaja[temp2][i].y - 5, punktyJaja[temp2][i].z);
+					glVertex3f(punktyJaja[temp2 - 1][i + 1].x, punktyJaja[temp2 - 1][i + 1].y - 5, punktyJaja[temp2 - 1][i + 1].z);
+				}
+			}
+			// Poziom
+			glVertex3f(punktyJaja[temp1][max].x, punktyJaja[temp1][max].y - 5, punktyJaja[temp1][max].z);
+			glVertex3f(punktyJaja[temp2][min].x, punktyJaja[temp2][min].y - 5, punktyJaja[temp2][min].z);
+
+			glVertex3f(punktyJaja[temp2][max].x, punktyJaja[temp2][max].y - 5, punktyJaja[temp2][max].z);
+			glVertex3f(punktyJaja[temp1][min].x, punktyJaja[temp1][min].y - 5, punktyJaja[temp1][min].z);
+
+			// Pion
+			glVertex3f(punktyJaja[temp2][max].x, punktyJaja[temp2][max].y - 5, punktyJaja[temp2][max].z);
+			glVertex3f(punktyJaja[temp2 - 1][max].x, punktyJaja[temp2 - 1][max].y - 5, punktyJaja[temp2 - 1][max].z);
+
+			glVertex3f(punktyJaja[temp1][max].x, punktyJaja[temp1][max].y - 5, punktyJaja[temp1][max].z);
+			glVertex3f(punktyJaja[temp1 + 1][max].x, punktyJaja[temp1 + 1][max].y - 5, punktyJaja[temp1 + 1][max].z);
+
+			// Skos
+			glVertex3f(punktyJaja[temp2][max].x, punktyJaja[temp2][max].y - 5, punktyJaja[temp2][max].z);
+			glVertex3f(punktyJaja[temp1 + 1][min].x, punktyJaja[temp1 + 1][min].y - 5, punktyJaja[temp1 + 1][min].z);
+
+			glVertex3f(punktyJaja[temp1][max].x, punktyJaja[temp1][max].y - 5, punktyJaja[temp1][max].z);
+			glVertex3f(punktyJaja[temp2 - 1][min].x, punktyJaja[temp2 - 1][min].y - 5, punktyJaja[temp2 - 1][min].z);
+
+			pom++;
+		}
+		glEnd();
+	}
+	// Kolorki
+	else if (model == 3) {
+		int pom = 0;
+		int min = 1;
+		int max = N - 1;
+		glBegin(GL_TRIANGLES);
+		while (pom != N){
+			int temp1 = N - pom + 1;
+			for (int i = 0; i < N - 1; i++) {
+				// Polowa 1
+				glColor3f(koloryJaja[temp1][i].x, koloryJaja[temp1][i].y, koloryJaja[temp1][i].z);
+				glVertex3f(punktyJaja[temp1][i].x, punktyJaja[temp1][i].y - 5, punktyJaja[temp1][i].z);
+				glColor3f(koloryJaja[temp1][i + 1].x, koloryJaja[temp1][i + 1].y, koloryJaja[temp1][i + 1].z);
+				glVertex3f(punktyJaja[temp1][i + 1].x, punktyJaja[temp1][i + 1].y - 5, punktyJaja[temp1][i + 1].z);
+				glColor3f(koloryJaja[temp1 + 1][i].x, koloryJaja[temp1 + 1][i].y, koloryJaja[temp1 + 1][i].z);
+				glVertex3f(punktyJaja[temp1 + 1][i].x, punktyJaja[temp1 + 1][i].y - 5, punktyJaja[temp1 + 1][i].z);
+
+				glColor3f(koloryJaja[temp1 + 1][i].x, koloryJaja[temp1 + 1][i].y, koloryJaja[temp1 + 1][i].z);
+				glVertex3f(punktyJaja[temp1 + 1][i].x, punktyJaja[temp1 + 1][i].y - 5, punktyJaja[temp1 + 1][i].z);
+				glColor3f(koloryJaja[temp1 + 1][i + 1].x, koloryJaja[temp1 + 1][i + 1].y, koloryJaja[temp1 + 1][i + 1].z);
+				glVertex3f(punktyJaja[temp1 + 1][i + 1].x, punktyJaja[temp1 + 1][i + 1].y - 5, punktyJaja[temp1 + 1][i + 1].z);
+				glColor3f(koloryJaja[temp1][i + 1].x, koloryJaja[temp1][i + 1].y, koloryJaja[temp1][i + 1].z);
+				glVertex3f(punktyJaja[temp1][i + 1].x, punktyJaja[temp1][i + 1].y - 5, punktyJaja[temp1][i + 1].z);
+			}
+			int temp2 = pom + 1;
+			for (int i = 0; i < N - 1; i++) {
+				// Polowa 2
+				glColor3f(koloryJaja[temp2][i].x, koloryJaja[temp2][i].y, koloryJaja[temp2][i].z);
+				glVertex3f(punktyJaja[temp2][i].x, punktyJaja[temp2][i].y - 5, punktyJaja[temp2][i].z);
+				glColor3f(koloryJaja[temp2][i + 1].x, koloryJaja[temp2][i + 1].y, koloryJaja[temp2][i + 1].z);
+				glVertex3f(punktyJaja[temp2][i + 1].x, punktyJaja[temp2][i + 1].y - 5, punktyJaja[temp2][i + 1].z);
+				glColor3f(koloryJaja[temp2 - 1][i].x, koloryJaja[temp2 - 1][i].y, koloryJaja[temp2 - 1][i].z);
+				glVertex3f(punktyJaja[temp2 - 1][i].x, punktyJaja[temp2 - 1][i].y - 5, punktyJaja[temp2 - 1][i].z);
+
+				glColor3f(koloryJaja[temp2 - 1][i].x, koloryJaja[temp2 - 1][i].y, koloryJaja[temp2 - 1][i].z);
+				glVertex3f(punktyJaja[temp2 - 1][i].x, punktyJaja[temp2 - 1][i].y - 5, punktyJaja[temp2 - 1][i].z);
+				glColor3f(koloryJaja[temp2 - 1][i + 1].x, koloryJaja[temp2 - 1][i + 1].y, koloryJaja[temp2 - 1][i + 1].z);
+				glVertex3f(punktyJaja[temp2 - 1][i + 1].x, punktyJaja[temp2 - 1][i + 1].y - 5, punktyJaja[temp2 - 1][i + 1].z);
+				glColor3f(koloryJaja[temp2][i + 1].x, koloryJaja[temp2][i + 1].y, koloryJaja[temp2][i + 1].z);
+				glVertex3f(punktyJaja[temp2][i + 1].x, punktyJaja[temp2][i + 1].y - 5, punktyJaja[temp2][i + 1].z);
+			}
+			
+			// Dorobki
+			glColor3f(koloryJaja[temp1][max].x, koloryJaja[temp1][max].y, koloryJaja[temp1][max].z);
+			glVertex3f(punktyJaja[temp1][max].x, punktyJaja[temp1][max].y - 5, punktyJaja[temp1][max].z);
+			glColor3f(koloryJaja[temp2][min].x, koloryJaja[temp2][min].y, koloryJaja[temp2][min].z);
+			glVertex3f(punktyJaja[temp2][min].x, punktyJaja[temp2][min].y - 5, punktyJaja[temp2][min].z);
+			glColor3f(koloryJaja[temp1 + 1][max].x, koloryJaja[temp1 + 1][max].y, koloryJaja[temp1 + 1][max].z);
+			glVertex3f(punktyJaja[temp1 + 1][max].x, punktyJaja[temp1 + 1][max].y - 5, punktyJaja[temp1 + 1][max].z);
+
+			glColor3f(koloryJaja[temp1 + 1][max].x, koloryJaja[temp1 + 1][max].y, koloryJaja[temp1 + 1][max].z);
+			glVertex3f(punktyJaja[temp1 + 1][max].x, punktyJaja[temp1 + 1][max].y - 5, punktyJaja[temp1 + 1][max].z);
+			glColor3f(koloryJaja[temp2 - 1][min].x, koloryJaja[temp2 - 1][min].y, koloryJaja[temp2 - 1][min].z);
+			glVertex3f(punktyJaja[temp2 - 1][min].x, punktyJaja[temp2 - 1][min].y - 5, punktyJaja[temp2 - 1][min].z);
+			glColor3f(koloryJaja[temp2][min].x, koloryJaja[temp2][min].y, koloryJaja[temp2][min].z);
+			glVertex3f(punktyJaja[temp2][min].x, punktyJaja[temp2][min].y - 5, punktyJaja[temp2][min].z);
+
+			glColor3f(koloryJaja[temp1][min].x, koloryJaja[temp1][min].y, koloryJaja[temp1][min].z);
+			glVertex3f(punktyJaja[temp1][min].x, punktyJaja[temp1][min].y - 5, punktyJaja[temp1][min].z);
+			glColor3f(koloryJaja[temp2][max].x, koloryJaja[temp2][max].y, koloryJaja[temp2][max].z);
+			glVertex3f(punktyJaja[temp2][max].x, punktyJaja[temp2][max].y - 5, punktyJaja[temp2][max].z);
+			glColor3f(koloryJaja[temp1 + 1][min].x, koloryJaja[temp1 + 1][min].y, koloryJaja[temp1 + 1][min].z);
+			glVertex3f(punktyJaja[temp1 + 1][min].x, punktyJaja[temp1 + 1][min].y - 5, punktyJaja[temp1 + 1][min].z);
+
+			glColor3f(koloryJaja[temp1 + 1][min].x, koloryJaja[temp1 + 1][min].y, koloryJaja[temp1 + 1][min].z);
+			glVertex3f(punktyJaja[temp1 + 1][min].x, punktyJaja[temp1 + 1][min].y - 5, punktyJaja[temp1 + 1][min].z);
+			glColor3f(koloryJaja[temp2 - 1][max].x, koloryJaja[temp2 - 1][max].y, koloryJaja[temp2 - 1][max].z);
+			glVertex3f(punktyJaja[temp2 - 1][max].x, punktyJaja[temp2 - 1][max].y - 5, punktyJaja[temp2 - 1][max].z);
+			glColor3f(koloryJaja[temp2][max].x, koloryJaja[temp2][max].y, koloryJaja[temp2][max].z);
+			glVertex3f(punktyJaja[temp2][max].x, punktyJaja[temp2][max].y - 5, punktyJaja[temp2][max].z);
+
+			pom++;
+		}
+			
+		glEnd();
+	}
+	else if (model == 4) {
+		glutWireTeapot(3.0); // Narysowanie obrazu czajnika do herbaty
+	}
+
+	glFlush();
+	// Przekazanie poleceń rysujących do wykonania
+
+	glutSwapBuffers();
+	//
+}
+
+void keys(unsigned char key, int x, int y)
+{
+	if (key == 'q') model = 1;
+	if (key == 'w') model = 2;
+	if (key == 'e') model = 3;
+	if (key == 'r') model = 4;
+	if (key == 'z') ruch = 1;
+	if (key == 'x') ruch = 2;
+	if (key == 'c') ruch = 3;
+
+	RenderScene(); // przerysowanie obrazu sceny
+}
+
+/*************************************************************************************/
+// Funkcja ustalająca stan renderowania
+
+void MyInit(void)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	// Kolor czyszcący (wypełnienia okna) ustawiono na czarny
+}
+/*************************************************************************************/
+// Funkcja ma za zadanie utrzymanie stałych proporcji rysowanych 
+// w przypadku zmiany rozmiarów okna.
+// Parametry vertical i horizontal (wysokość i szerokość okna) są 
+// przekazywane do funkcji za każdym razem gdy zmieni się rozmiar okna.
+
+void ChangeSize(GLsizei horizontal, GLsizei vertical)
+{
+	GLfloat AspectRatio;
+	// Deklaracja zmiennej AspectRatio  określającej proporcję
+	// wymiarów okna 
+	if (vertical == 0)  // Zabezpieczenie przed dzieleniem przez 0
+		vertical = 1;
+	glViewport(0, 0, horizontal, vertical);
+	// Ustawienie wielkościokna okna widoku (viewport)
+	// W tym przypadku od (0,0) do (horizontal, vertical)  
+	glMatrixMode(GL_PROJECTION);
+	// Przełączenie macierzy bieżącej na macierz projekcji 
+	glLoadIdentity();
+	// Czyszcznie macierzy bieżącej            
+	AspectRatio = (GLfloat)horizontal / (GLfloat)vertical;
+	// Wyznaczenie współczynnika  proporcji okna
+	// Gdy okno nie jest kwadratem wymagane jest określenie tak zwanej
+	// przestrzeni ograniczającej pozwalającej zachować właściwe
+	// proporcje rysowanego obiektu.
+	// Do okreslenia przestrzeni ograniczjącej służy funkcja
+	// glOrtho(...)            
+	if (horizontal <= vertical)
+		glOrtho(-7.5, 7.5, -7.5 / AspectRatio, 7.5 / AspectRatio, 10.0, -10.0);
+	else
+		glOrtho(-7.5*AspectRatio, 7.5*AspectRatio, -7.5, 7.5, 10.0, -10.0);
+	glMatrixMode(GL_MODELVIEW);
+	// Przełączenie macierzy bieżącej na macierz widoku modelu                                   
+	glLoadIdentity();
+	// Czyszcenie macierzy bieżącej
+}
+/*************************************************************************************/
+// Główny punkt wejścia programu. Program działa w trybie konsoli
+
+void main(void)
+{
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+	glutInitWindowSize(1000, 1000);
+
+	glutCreateWindow("Układ współrzędnych 3-D");
+
+	nic();
+
+	glutDisplayFunc(RenderScene);
+	// Określenie, że funkcja RenderScene będzie funkcją zwrotną
+	// (callback function).  Bedzie ona wywoływana za każdym razem 
+	// gdy zajdzie potrzba przeryswania okna 
+	glutReshapeFunc(ChangeSize);
+	// Dla aktualnego okna ustala funkcję zwrotną odpowiedzialną
+	// zazmiany rozmiaru okna      
+	MyInit();
+	// Funkcja MyInit() (zdefiniowana powyżej) wykonuje wszelkie
+	// inicjalizacje konieczne  przed przystąpieniem do renderowania
+	glEnable(GL_DEPTH_TEST);
+	// Włączenie mechanizmu usuwania powierzchni niewidocznych
+
+	glutKeyboardFunc(keys);
+
+	//glutIdleFunc(spinEgg);
+
+	glutMainLoop();
+	// Funkcja uruchamia szkielet biblioteki GLUT
+}
+/*************************************************************************************/
